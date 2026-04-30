850fbeae2cd826167134a512b3cec8e14a88c96c
01Karry <ilabogatko@gmail.com>
2026-04-30 21:15:37 +0300

    Убрал из rmUtils функции, которые пока не исполльзовались

--- src/utils/rmUtils.c
+++ src/utils/rmUtils.c
@@ -6,46 +6,6 @@ void rmFile(char* path) {
     remove(path);
 }
 
-i8 rmNEDir(char* path) {
-    DIR* pDir = opendir(path);
-    if (pDir == NULL) return -1;
-
-    for (struct dirent* entry = readdir(pDir); entry != NULL; entry = readdir(pDir)) {
-        if (isIgnore(path, entry->d_name) == 0) {
-            char fullPath[PATH_SIZE];
-            sprintf(fullPath, "%s\\%s", path, entry->d_name);
-
-            rmAny(fullPath);
-        }
-    }
-
-    closedir(pDir);
-
-    rmdir(path);
-
-    return 0;
-}
-
-i8 rmAny(char* path) {
-    if (isIgnore(".", path) != 0) {
-        printf("ignore\n");
-        return 0;
-    }
-
-    i8 type = getType(path);
-
-    switch (type) {
-    case 0:
-        rmFile(path);
-        break;
-    case 1:
-        return rmNEDir(path);
-        break;
-    }
-
-    return 0;
-}
-
 i8 baseIgnoreRmNEDir(char* path) {
     DIR* pDir = opendir(path);
     if (pDir == NULL) return -1;
--- src/utils/rmUtils.h
+++ src/utils/rmUtils.h
@@ -4,7 +4,5 @@
 #include "defs.h"
 
 void rmFile(char* path);
-i8 rmNEDir(char* path);
-i8 rmAny(char* path);
 i8 baseIgnoreRmNEDir(char* path);
 void baseIgnoreRmAny(char* path);
